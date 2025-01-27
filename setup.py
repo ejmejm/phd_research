68b5b7bba46fc8c6511dc6803fb7ed69eaa65820
ejmejm <ninjaedan@aol.com>
2025-01-27 15:30:02 -0600

    Resturctured

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="discovery",
+    name="phd_research",
     version="0.1.0",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     author="Edan Meyer",
     author_email="ejmejm98@gmail.com",
-    description="Research on the disovery problem implemented with JAX and Equinox",
+    description="Research for my PhD, mainly on the discovery problem",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/ejmejm/discovery",
+    url="https://github.com/ejmejm/phd_research",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
