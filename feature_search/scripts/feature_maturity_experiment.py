2e570426a0ac572615236224f5923c4ba64aa397
ejmejm <ninjaedan@aol.com>
2025-03-27 22:58:46 +0900

    Start for feature maturity script

---
+++ feature_search/scripts/feature_maturity_experiment.py
@@ -0,0 +1,159 @@
+"""
+This script is used to run experiments for better understanding how long it takes for new
+features to mature and converge.
+More specifically, the goal of this script is to produce metrics that explain how long it
+takes for a feature to become "safe", and how long it takes for the associated weight to converge
+as a function of the optimal utility of the feature and the number of other existing features.
+"""
+
+
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import numpy as np
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+
+import wandb
+import hydra
+from omegaconf import DictConfig
+
+from feature_recycling import reset_input_weights
+from idbd import IDBD, RMSPropIDBD
+from models import LTU
+from tasks import NonlinearGEOFFTask
+from run_experiment import *
+
+
+@hydra.main(config_path='../conf', config_name='feature_maturity_defaults')
+def main(cfg: DictConfig) -> None:
+    """Run the feature recycling experiment."""
+    task, task_iterator, model, criterion, optimizer, recycler, cbp_tracker = \
+        prepare_components(cfg)
+
+    assert isinstance(task, NonlinearGEOFFTask)
+    
+    assert cfg.model.weight_init_method == 'binary', \
+        "Binary weight initialization is required for reproducing Mahmood and Sutton (2013)"
+    assert cfg.task.weight_init == 'binary', \
+        "Binary weight initialization is required for reproducing Mahmood and Sutton (2013)"
+    assert cfg.model.activation == 'ltu', \
+        "LTU activations are required for reproducing Mahmood and Sutton (2013)"
+    assert cfg.task.activation == 'ltu', \
+        "LTU activations are required for reproducing Mahmood and Sutton (2013)"
+
+    if cbp_tracker is not None:
+        cbp_tracker.incoming_weight_init = 'binary'
+    
+    # Init target output weights to kaiming uniform and predictor output weights to zero
+    torch.nn.init.kaiming_uniform_(
+        task.weights[-1],
+        mode = 'fan_in',
+        nonlinearity = 'linear',
+    )
+    torch.nn.init.zeros_(model.layers[-1].weight)
+    
+    # Change LTU threshold for target and predictors
+    ltu_threshold = 0.1 * cfg.task.n_features
+    for layer in model.layers:
+        if isinstance(layer, LTU):
+            layer.threshold = ltu_threshold
+    task.activation_fn.threshold = ltu_threshold
+
+    # Training loop
+    step = 0
+    pbar = tqdm(total=cfg.train.total_steps, desc='Training')
+    
+    # Initialize accumulators
+    cumulative_loss = np.float128(0.0)
+    loss_acc = 0.0
+    accuracy_acc = 0.0
+    n_steps_since_log = 0
+    cumulant_mean = 0.0
+    cumulant_square_mean = 0.0
+    total_pruned = 0
+    cumulant_gamma = 0.999
+    target_buffer = []
+    
+    while step < cfg.train.total_steps:
+        # Generate batch of data
+        inputs, targets = next(task_iterator)
+        target_buffer.extend(targets.view(-1).tolist())
+        features, targets = inputs.to(cfg.device), targets.to(cfg.device)
+        
+        if cfg.train.standardize_cumulants:
+            with torch.no_grad():
+                targets, cumulant_mean, cumulant_square_mean = standardize_targets(
+                    targets, cumulant_mean, cumulant_square_mean, cumulant_gamma, step)
+
+        # Reset weights and optimizer states for recycled features
+        if cbp_tracker is not None:
+            pruned_idxs = cbp_tracker.prune_features()
+            total_pruned += sum([len(idxs) for idxs in pruned_idxs.values()])
+
+        # Forward pass
+        outputs, param_inputs = model(features)
+        loss = criterion(outputs, targets)
+        
+        # loss += torch.randn_like(loss)
+
+        # Backward pass
+        optimizer.zero_grad()
+        if isinstance(optimizer, RMSPropIDBD):
+            loss.backward(create_graph=True)
+            # Mean over batch dimension
+            param_inputs = {k: v.mean(dim=0) for k, v in param_inputs.items()}
+            optimizer.step(param_inputs)
+        elif isinstance(optimizer, IDBD):
+            # Mean over batch dimension
+            param_inputs = {k: v.mean(dim=0) for k, v in param_inputs.items()}
+            optimizer.step(loss, outputs, param_inputs)
+        else:
+            loss.backward()
+            optimizer.step()
+        
+        # Accumulate metrics
+        loss_acc += loss.item()
+        cumulative_loss += loss.item()
+        n_steps_since_log += 1
+        
+        # Calculate and accumulate accuracy for classification
+        if isinstance(criterion, nn.CrossEntropyLoss):
+            _, predicted = outputs.max(1)
+            accuracy = predicted.eq(targets).float().mean().item()
+            accuracy_acc += accuracy
+        
+        # Log metrics
+        if step % cfg.train.log_freq == 0:
+            metrics = {
+                'step': step,
+                'samples': step * cfg.train.batch_size,
+                'loss': loss_acc / n_steps_since_log,
+                'cumulative_loss': cumulative_loss,
+                'accuracy': accuracy_acc / n_steps_since_log if isinstance(criterion, nn.CrossEntropyLoss) else None,
+                'squared_targets': torch.tensor(target_buffer).square().mean().item(),
+                'units_pruned': total_pruned,
+            }
+            # Add model statistics
+            metrics.update(get_model_statistics(model, features, param_inputs))
+            wandb.log(metrics)
+            
+            pbar.set_postfix(loss=metrics['loss'], accuracy=metrics['accuracy'])
+            
+            # Reset accumulators
+            loss_acc = 0.0
+            accuracy_acc = 0.0
+            n_steps_since_log = 0
+            target_buffer = []
+
+        step += 1
+        pbar.update(1)
+    
+    pbar.close()
+    wandb.finish()
+
+
+if __name__ == '__main__':
+    main()
