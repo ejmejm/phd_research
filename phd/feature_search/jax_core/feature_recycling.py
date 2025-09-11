451d0004eab78d392b7a62110eff11e0b4448aec
ejmejm <ninjaedan@aol.com>
2025-09-10 18:37:39 -0600

    JAX CBP more progress

--- phd/feature_search/jax_core/feature_recycling.py
+++ phd/feature_search/jax_core/feature_recycling.py
@@ -10,8 +10,8 @@ from .optimizer import EqxOptimizer
 
 
 class FeatureStats(eqx.Module):
-    age: Int[Array, '']
-    utility: Float[Array, '']
+    age: Int[Array, 'n_features']
+    utility: Float[Array, 'n_features']
 
 
 class CBPTracker(eqx.Module):
@@ -27,10 +27,12 @@ class CBPTracker(eqx.Module):
     replace_rate: Float[Array, '']
     decay_rate: Float[Array, '']
     maturity_threshold: Int[Array, '']
+    all_feature_stats: PyTree # Pytree with FeatureStats for leaves
     rng: PRNGKeyArray
     
     def __init__(
         self,
+        model: eqx.Module,
         replace_rate: float = 1e-4,
         decay_rate: float = 0.99,
         maturity_threshold: int = 100,
@@ -38,6 +40,7 @@ class CBPTracker(eqx.Module):
         outgoing_weight_init: str = 'zeros', # {'zeros', 'kaiming_uniform'}
         utility_reset_mode: str = 'median', # {'median', 'zero'}
         initial_step_size_method: str = 'constant', # {'constant', 'mean', 'median'}
+        filter_spec: Optional[PyTree] = None,
         rng: Optional[PRNGKeyArray] = None,
     ):
         assert utility_reset_mode in {'median', 'zero'}
@@ -45,8 +48,20 @@ class CBPTracker(eqx.Module):
         assert outgoing_weight_init in {'zeros', 'kaiming_uniform'}
         assert initial_step_size_method in {'constant', 'mean', 'median'}
         
-        if rng is None:
-            rng = jax.random.PRNGKey(random.randint(0, 2**31))
+        if filter_spec is not None:
+            model = eqx.filter(model, filter_spec)
+        
+        is_linear_weights = lambda x: isinstance(x, Array) and x.ndim == 2 
+        assert jnp.all(jnp.array(jax.tree.leaves(jax.tree.map(lambda x: is_linear_weights(x), model)))), \
+            "All layers must be 2D weight matrices"
+        
+        self.all_feature_stats = jax.tree.map(
+            lambda weights: FeatureStats(
+                age = jnp.zeros(weights.shape[1], dtype=jnp.int32),
+                utility = jnp.zeros(weights.shape[1], dtype=jnp.float32)
+            ),
+            tree = model,
+        )
         
         self.incoming_weight_init = incoming_weight_init
         self.outgoing_weight_init = outgoing_weight_init
@@ -56,22 +71,51 @@ class CBPTracker(eqx.Module):
         self.replace_rate = jnp.array(replace_rate, dtype=jnp.float32)
         self.decay_rate = jnp.array(decay_rate, dtype=jnp.float32)
         self.maturity_threshold = jnp.array(maturity_threshold, dtype=jnp.int32)
+        
+        if rng is None:
+            rng = jax.random.PRNGKey(random.randint(0, 2**31))
         self.rng = rng
     
     def prune_layer_features(
         self,
-        layer: eqx.Module,
+        weights: Array,
         path: Tuple[...], # Of types GetArrKey (jax.tree_util.GetAttrKey)
+        optimizer: Optional[EqxOptimizer] = None,
     ) -> Array:
-        pass
+        assert weights.ndim == 2, "Weights must be 2D"
+        n_features = weights.shape[1]
+        
+        # Update age
+        # Update utility
+        # Update replacement accumulator
+        
+        # Get indices to reinitialize (prune mask)
+        prune_mask = jnp.zeros(layer.in_features, dtype=jnp.bool_)
+        
+        # Reset stats for those features
+        # Reinit input and output weights for given features
+        
+        # Reinit optimizer input and output weight states for given features
+        
+        return prune_mask
     
     def prune_features(
         self,
         model: eqx.Module,
+        activation_values: eqx.Module,
         optimizer: Optional[EqxOptimizer] = None,
-        filter_spec: Optional[PyTree] = None,
     ) -> eqx.Module:
-        """Prune features based on CBP utility and return a mask over the features reset."""
+        """Prune features based on CBP utility and return a mask over the features reset.
+        
+        Args:
+            model: The full model to prune
+            activation_values: Pytree matching the structure of model with the activation values for each layer
+            optimizer: The optimizer optimizing the given model
+            filter_spec: Boolean Pytree matching the structure of model with True for prunable layers
+            
+        Returns:
+            A mask over the features reset
+        """
         reset_idxs = {}
         for layer in self._tracked_layers.keys():
             self._step_replacement_accumulator(layer)
