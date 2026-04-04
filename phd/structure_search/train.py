554e9f49132d6d6e9b0946bc0871dfe76face18f
ejmejm <ninjaedan@aol.com>
2026-04-04 00:05:25 -0600

    Revert "Add pluggable connection utility functions (contribution + propagated)"
    This reverts commit fdaee4e776495ba2aa63ddd7e6bcda5f4d54daf8.

--- phd/structure_search/train.py
+++ phd/structure_search/train.py
@@ -42,7 +42,6 @@ from phd.structure_search.connectivity_manager import (
     ConnectionConnectivityManager,
     full_input_generate,
     contribution_utility, upgd_utility, si_utility, loo_utility,
-    contribution_connection_utility, propagated_connection_utility,
 )
 from phd.structure_search.dynamic_network import (
     DynamicNetwork, sync_outgoing_weights, build_outgoing_indices,
@@ -245,12 +244,6 @@ def prepare_experiment(
             tracker_mode = cfg.structure_tracker.get('mode', 'unit')
 
             if tracker_mode == 'connection':
-                conn_util_fn_map = {
-                    'contribution': contribution_connection_utility,
-                    'propagated': propagated_connection_utility,
-                }
-                conn_util_fn = conn_util_fn_map.get(
-                    cfg.structure_tracker.get('connection_utility_fn', 'contribution'))
                 tracker = ConnectionConnectivityManager(
                     model=model,
                     prune_rate=cfg.structure_tracker.prune_rate,
@@ -263,7 +256,6 @@ def prepare_experiment(
                     output_weight_init=cfg.structure_tracker.get(
                         'output_weight_init', 'zero'),
                     generate_fn=generate_fn,
-                    connection_utility_fn=conn_util_fn,
                     rng=rng_from_string(rng, 'tracker'),
                 )
             else:
